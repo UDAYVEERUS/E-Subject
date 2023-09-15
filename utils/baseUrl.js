1b8e2af6e8cc9705951b37a212f9d5cd698afe5a
UDAYVEERUS <udayveerus348567@gmail.com>
2023-09-15 18:55:12 +0530

    cahnge the base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://cofly-ecd7huzhf-udayveerus.vercel.app"
-		: "https://cofly-ecd7huzhf-udayveerus.vercel.app";
+		? "http://localhost:3000"
+		: "http://localhost:3000";
 
 export default baseUrl;

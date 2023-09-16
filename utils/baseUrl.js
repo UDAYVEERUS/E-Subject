36b428cf746366a464d0f94e3ebd153d1d6f3783
UDAYVEERUS <udayveerus348567@gmail.com>
2023-09-16 12:49:59 +0530

    change url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://elearniv-react.envytheme.com"
+		? "https://www.cofly.in"
 		: "http://localhost:3000";
 
 export default baseUrl;

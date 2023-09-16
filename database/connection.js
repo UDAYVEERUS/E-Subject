a1471c3c49b1615d132dbd2861f145762a65bbc4
UDAYVEERUS <udayveerus348567@gmail.com>
2023-09-16 12:34:35 +0530

    change config

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,18 @@
 import Sequelize from "sequelize";
 import config from "./config/config.mjs";
-
+import mysql2 from "mysql2"
 let sequelize;
 if (process.env.NODE_ENV === "production") {
-	sequelize = new Sequelize(config.production);
+	sequelize = new Sequelize({
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD_PROD,
+		database: process.env.DB_NAME_PRODUCTION,
+		host: process.env.DB_HOSTNAME,
+		port: process.env.DB_PORT_PROD,
+		logging: false,
+		dialect: "mysql",
+		dialectModule: mysql2
+	});
 } else {
 	sequelize = new Sequelize(config.development);
 }
